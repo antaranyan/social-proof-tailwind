8edf2aa751ba552e4882b2f6c01ca9f2e7851d32
Sumaer Jolly <sumaerjolly@gmail.com>
2020-10-01 16:24:27 +0530

    set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Spartan'],
+      },
+      colors: {
+        magenta: '#8E398E',
+        softPink: '#EE68A4',
+        darkMagenta: '#937B92',
+        lightMagenta: '#F7F2F7',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
